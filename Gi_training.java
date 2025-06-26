5bc771130c1abca5f19cebcf65f72eaa03d86b75
original2011 <oriyuda810@gmail.com>
2025-06-26 19:08:09 +0300

    test

--- Gi_training.java
+++ Gi_training.java
@@ -1,7 +1,6 @@
 public class Gi_training {
-    while(1 > 0)
     public static void main(String[] args) {
         System.out.println("hello world");
-
+        System.out.println("one is not two");
     }
 }
